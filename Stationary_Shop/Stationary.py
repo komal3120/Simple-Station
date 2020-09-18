49f9144b5aa6b2744bd4c966bc71d604a6168e1a
komal <komalpal0000@gmail.com>
2020-09-19 00:14:41 +0530

    Image Slider Added

--- Stationary_Shop/Stationary.py
+++ Stationary_Shop/Stationary.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 app=Flask(__name__)
 
+@app.route("/Home")
 @app.route("/")
 def home():
-	return render_template('layout.html')
+	return render_template('home.html')
 
 
 app.run(debug=True)
